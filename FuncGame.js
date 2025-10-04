26ccb1d9b201387efc029d9f4fdbd53cbea8d2f4
Hamanilol <abdulrahmanmusa325@gmail.com>
2025-10-04 23:44:28 +0300

    16autisticclowns

--- FuncGame.js
+++ FuncGame.js
@@ -21,117 +21,118 @@ document.addEventListener("keydown", (controls) => {
   }
 });
 
-
+// Pipe generator
 const generatePipes = () => {
   if (isGameOver) return;
 
-  const gap = 140;
-  const minPipeHeight = 50;
+  const gap = 180;
+  const minPipeHeight = 80;
   const maxPipeHeight = gameArea.clientHeight - gap - minPipeHeight;
-  const topPipeHeight = Math.floor(Math.random() * (maxPipeHeight - minPipeHeight)) + minPipeHeight;
+  const topPipeHeight = Math.random() * (maxPipeHeight - minPipeHeight) + minPipeHeight;
+  // used stackoverflow.com to learn this particular line to randomise the height of pipes  https://stackoverflow.com/questions/1527803/generating-random-whole-numbers-in-javascript-in-a-specific-range/1527820#1527820
   const bottomPipeHeight = gameArea.clientHeight - topPipeHeight - gap;
 
   const topPipe = document.createElement("img");
   topPipe.src = "./FlappyBird/toppipe.png";
   topPipe.classList.add("upperpipe");
-  topPipe.style.height = `${topPipeHeight}px`;
-  topPipe.style.left = `${gameArea.clientWidth}px`;
+  topPipe.style.height = topPipeHeight + "px";
+  topPipe.style.left = gameArea.clientWidth + "px";
 
   const bottomPipe = document.createElement("img");
   bottomPipe.src = "./FlappyBird/bottompipe.png";
   bottomPipe.classList.add("lowerpipe");
-  bottomPipe.style.height = `${bottomPipeHeight}px`;
-  bottomPipe.style.left = `${gameArea.clientWidth}px`;
+  bottomPipe.style.height = bottomPipeHeight + "px";
+  bottomPipe.style.left = gameArea.clientWidth + "px";
 
   gameArea.appendChild(topPipe);
   gameArea.appendChild(bottomPipe);
 
-  pipes.push(topPipe, bottomPipe);
+  pipes.push({ top: topPipe, bottom: bottomPipe });
 };
  
 // Move pipes
 const movePipes = () => {
   pipes.forEach((pipePair, index) => {
-    const top = pipePair[0];
-    const bottom = pipePair[1];
+    const { top, bottom } = pipePair;
     const newLeft = top.offsetLeft - pipeSpeed;
 
     top.style.left = newLeft + "px";
     bottom.style.left = newLeft + "px";
-  });
+
     // remove off-screen pipes
     if (newLeft + top.offsetWidth < 0) {
       top.remove();
       bottom.remove();
-      pipes.filter(index, 1);
-  };
-};
-
-// Collision check
-const checkCollision = () => {
-  const birdRect = birdEl.getBoundingClientRect();
-
-  pipes.forEach(({ top, bottom }) => {
-    const topRect = top.getBoundingClientRect();
-    const bottomRect = bottom.getBoundingClientRect();
-
-    if (
-      (birdRect.left < topRect.right &&
-        birdRect.right > topRect.left &&
-        birdRect.top < topRect.bottom) ||
-      (birdRect.left < bottomRect.right &&
-        birdRect.right > bottomRect.left &&
-        birdRect.bottom > bottomRect.top)
-    ) {
-      isGameOver = true;
-      console.log("Hit a pipe!");
+      pipes.splice(index, 1); // i used mdn.com to learn splice
     }
   });
-
-  if (birdY + birdEl.offsetHeight >= gameArea.clientHeight) {
-    isGameOver = true;
-    console.log("Hit the ground!");
-  }
 };
 
-//ORRR
-//const checkCollision = () => {
-  // Bird position and size
-//   const birdLeft = birbaction.offsetLeft;
-//   const birdTop = birbaction.offsetTop;
-//   const birdRight = birdLeft + birbaction.offsetWidth;
-//   const birdBottom = birdTop + birbaction.offsetHeight;
+// // Collision check
+// const checkCollision = () => {
+//   const birdRect = birdEl.getBoundingClientRect();
 
-//   // Upper pipe position and size
-//   const upperLeft = upperpipes.offsetLeft;
-//   const upperTop = upperpipes.offsetTop;
-//   const upperRight = upperLeft + upperpipes.offsetWidth;
-//   const upperBottom = upperTop + upperpipes.offsetHeight;
+//   pipes.forEach(({ top, bottom }) => {
+//     const topRect = top.getBoundingClientRect();
+//     const bottomRect = bottom.getBoundingClientRect();
 
-//   // Lower pipe position and size
-//   const lowerLeft = lowerpipes.offsetLeft;
-//   const lowerTop = lowerpipes.offsetTop;
-//   const lowerRight = lowerLeft + lowerpipes.offsetWidth;
-//   const lowerBottom = lowerTop + lowerpipes.offsetHeight;
+//     if (
+//       (birdRect.left < topRect.right &&
+//         birdRect.right > topRect.left &&
+//         birdRect.top < topRect.bottom) ||
+//       (birdRect.left < bottomRect.right &&
+//         birdRect.right > bottomRect.left &&
+//         birdRect.bottom > bottomRect.top)
+//     ) {
+//       isGameOver = true;
+//       console.log("Hit a pipe!");
+//     }
+//   });
 
-//   // Check collision with upper pipe
-//   const hitUpper =
-//     birdRight > upperLeft &&
-//     birdLeft < upperRight &&
-//     birdTop < upperBottom;
-
-//   // Check collision with lower pipe
-//   const hitLower =
-//     birdRight > lowerLeft &&
-//     birdLeft < lowerRight &&
-//     birdBottom > lowerTop;
-
-// 
-//   if (hitUpper || hitLower) {
+//   if (birdY + birdEl.offsetHeight >= gameArea.clientHeight) {
 //     isGameOver = true;
-//     console.log("    Collision detected!");
+//     console.log("Hit the ground!");
 //   }
 // };
+
+//ORRR
+const checkCollision = () => {
+  //Bird position and size
+  const birdLeft = birdEl.offsetLeft;
+  const birdTop = birdEl.offsetTop;
+  const birdRight = birdEl + birbaction.offsetWidth;
+  const birdBottom = birdEl + birbaction.offsetHeight;
+
+  // Upper pipe position and size
+  const upperLeft = upperpipe.offsetLeft;
+  const upperTop = upperpipe.offsetTop;
+  const upperRight = upperLeft + upperpipe.offsetWidth;
+  const upperBottom = upperTop + upperpipe.offsetHeight;
+
+   //Lower pipe position and size
+  const lowerLeft = lowerpipe.offsetLeft;
+  const lowerTop = lowerpipe.offsetTop;
+  const lowerRight = lowerLeft + lowerpipe.offsetWidth;
+  const lowerBottom = lowerTop + lowerpipe.offsetHeight;
+
+  // Check collision with upper pipe
+  const hitUpper =
+    birdRight > upperLeft &&
+    birdLeft < upperRight &&
+    birdTop < upperBottom;
+
+   //Check collision with lower pipe
+  const hitLower =
+    birdRight > lowerLeft &&
+    birdLeft < lowerRight &&
+    birdBottom > lowerTop;
+
+  //If collision detected
+  if (hitUpper || hitLower) {
+    isGameOver = true;
+    console.log("Collision detected!");
+  }
+};
 
 // Main loop
 const gameLoop = () => {
@@ -152,5 +153,5 @@ const gameLoop = () => {
 };
 
 // Start
-setInterval(generatePipes, 2000);
+setInterval(generatePipes, 4000);
 requestAnimationFrame(gameLoop);
