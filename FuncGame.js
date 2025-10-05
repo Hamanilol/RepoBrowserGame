3d2ed9a636b3e6837387b9085fccc06da2408b36
Hamanilol <abdulrahmanmusa325@gmail.com>
2025-10-05 03:10:53 +0300

    commitagain

--- FuncGame.js
+++ FuncGame.js
@@ -1,10 +1,10 @@
 // Grab elements
-const birdEl = document.querySelector(".birbs");
+const birb = document.querySelector(".birbs");
 const gameArea = document.getElementById("container");
 
 // Bird physics
 let birdX = 100;
-let birdY = 100;
+let birdY = 500;
 let velocity = 0;
 const gravity = 0.5;
 const jumpStrength = -8;
@@ -20,18 +20,28 @@ document.addEventListener("keydown", (controls) => {
     velocity = jumpStrength;
   }
 });
-
-// Pipe generator
+  // Create pipe elements whenever this function is called
 const generatePipes = () => {
   if (isGameOver) return;
 
+// Gap and pipe heights
   const gap = 180;
+
+  // minimum height of the top pipe
   const minPipeHeight = 80;
+
+  // maximum height of the top pipe
   const maxPipeHeight = gameArea.clientHeight - gap - minPipeHeight;
+
+  // random height for the top pipe between min and max height
   const topPipeHeight = Math.random() * (maxPipeHeight - minPipeHeight) + minPipeHeight;
-  // used stackoverflow.com to learn this particular line to randomise the height of pipes  https://stackoverflow.com/questions/1527803/generating-random-whole-numbers-in-javascript-in-a-specific-range/1527820#1527820
+
+  // i used stackoverflow.com to learn this particular line to demonstrate the random heights for the pipes in the range of max and minimum heights  https://stackoverflow.com/questions/1527803/generating-random-whole-numbers-in-javascript-in-a-specific-range/1527820#1527820
   const bottomPipeHeight = gameArea.clientHeight - topPipeHeight - gap;
 
+
+
+  // I used mdn.com to learn createElement https://developer.mozilla.org/en-US/docs/Web/API/Document/createElement
   const topPipe = document.createElement("img");
   topPipe.src = "./FlappyBird/toppipe.png";
   topPipe.classList.add("upperpipe");
@@ -46,7 +56,7 @@ const generatePipes = () => {
 
   gameArea.appendChild(topPipe);
   gameArea.appendChild(bottomPipe);
-
+// used mdn.com to learn appendChild https://developer.mozilla.org/en-US/docs/Web/API/Node/appendChild and i followed the other link to find out that you can append multiple elements
   pipes.push({ top: topPipe, bottom: bottomPipe });
 };
  
@@ -55,7 +65,7 @@ const movePipes = () => {
   pipes.forEach((pipePair, index) => {
     const { top, bottom } = pipePair;
     const newLeft = top.offsetLeft - pipeSpeed;
-
+    
     top.style.left = newLeft + "px";
     bottom.style.left = newLeft + "px";
 
@@ -63,95 +73,112 @@ const movePipes = () => {
     if (newLeft + top.offsetWidth < 0) {
       top.remove();
       bottom.remove();
-      pipes.splice(index, 1); // i used mdn.com to learn splice
+      pipes.splice(index, 1); // i used mdn.com to learn splice and push https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/splice push https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/push
     }
   });
 };
-
-// // Collision check
-// const checkCollision = () => {
-//   const birdRect = birdEl.getBoundingClientRect();
-
-//   pipes.forEach(({ top, bottom }) => {
-//     const topRect = top.getBoundingClientRect();
-//     const bottomRect = bottom.getBoundingClientRect();
-
-//     if (
-//       (birdRect.left < topRect.right &&
-//         birdRect.right > topRect.left &&
-//         birdRect.top < topRect.bottom) ||
-//       (birdRect.left < bottomRect.right &&
-//         birdRect.right > bottomRect.left &&
-//         birdRect.bottom > bottomRect.top)
-//     ) {
-//       isGameOver = true;
-//       console.log("Hit a pipe!");
-//     }
-//   });
-
-//   if (birdY + birdEl.offsetHeight >= gameArea.clientHeight) {
-//     isGameOver = true;
-//     console.log("Hit the ground!");
-//   }
-// };
-
-//ORRR
+const endGame = () => {
+  if (isGameOver) return;
+  isGameOver = true;
+  gameLostText();
+};
+// Collision check
 const checkCollision = () => {
-  //Bird position and size
-  const birdLeft = birdEl.offsetLeft;
-  const birdTop = birdEl.offsetTop;
-  const birdRight = birdEl + birbaction.offsetWidth;
-  const birdBottom = birdEl + birbaction.offsetHeight;
+  const birdRect = birb.getBoundingClientRect();
 
-  // Upper pipe position and size
-  const upperLeft = upperpipe.offsetLeft;
-  const upperTop = upperpipe.offsetTop;
-  const upperRight = upperLeft + upperpipe.offsetWidth;
-  const upperBottom = upperTop + upperpipe.offsetHeight;
+  pipes.forEach(({ top, bottom }) => {
+    const topRect = top.getBoundingClientRect();
+    const bottomRect = bottom.getBoundingClientRect();
 
-   //Lower pipe position and size
-  const lowerLeft = lowerpipe.offsetLeft;
-  const lowerTop = lowerpipe.offsetTop;
-  const lowerRight = lowerLeft + lowerpipe.offsetWidth;
-  const lowerBottom = lowerTop + lowerpipe.offsetHeight;
-
-  // Check collision with upper pipe
-  const hitUpper =
-    birdRight > upperLeft &&
-    birdLeft < upperRight &&
-    birdTop < upperBottom;
-
-   //Check collision with lower pipe
-  const hitLower =
-    birdRight > lowerLeft &&
-    birdLeft < lowerRight &&
-    birdBottom > lowerTop;
-
-  //If collision detected
-  if (hitUpper || hitLower) {
+    if (
+      (birdRect.left < topRect.right &&
+        birdRect.right > topRect.left &&
+        birdRect.top < topRect.bottom) ||
+      (birdRect.left < bottomRect.right &&
+        birdRect.right > bottomRect.left &&
+        birdRect.bottom > bottomRect.top)
+    ) {
+      isGameOver = true;
+      console.log("Hit a pipe!");
+    }
+  });
+// Check if bird hits the ground
+  if (birdY + birb.offsetHeight >= gameArea.clientHeight) {
     isGameOver = true;
-    console.log("Collision detected!");
+    console.log("Hit the ground!");
+  }
+  // hits the ceiling
+  if (birdY <= 0) {
+    isGameOver = true;
+    console.log("Hit the ceiling!");
   }
 };
+
+//ORRR
+// const checkCollision = () => {
+//   //Bird position and size
+//   const birdLeft = birdEl.offsetLeft;
+//   const birdTop = birdEl.offsetTop;
+//   const birdRight = birdLeft + birdEl.offsetWidth;
+//   const birdBottom = birdTop + birdEl.offsetHeight;
+
+//   // Pipe positions and sizes (assuming only one pair of pipes for simplicity)
+
+//   const upperLeft = topPipe.offsetLeft;
+//   const upperTop = topPipe.offsetTop;
+//   const upperRight = upperLeft + topPipe.offsetWidth;
+//   const upperBottom = upperTop + topPipe.offsetHeight;
+
+//   const lowerLeft = bottomPipe.offsetLeft;
+//   const lowerTop = bottomPipe.offsetTop;
+//   const lowerRight = lowerLeft + bottomPipe.offsetWidth;
+//   const lowerBottom = lowerTop + bottomPipe.offsetHeight;
+
+
+//   const hitUpper =
+//     birdRight > upperLeft &&
+//     birdLeft < upperRight &&
+//     birdTop < upperBottom;
+
+
+//   const hitLower =
+//     birdRight > lowerLeft &&
+//     birdLeft < lowerRight &&
+//     birdBottom > lowerTop;
+
+
+//   if (hitUpper || hitLower) {
+//     isGameOver = true;
+//     console.log("Collision detected!");
+//   }
+// };
 
 // Main loop
 const gameLoop = () => {
   if (!isGameOver) {
     velocity += gravity;
     birdY += velocity;
-
-    birdEl.style.top = `${birdY}px`;
-    birdEl.style.left = `${birdX}px`;
+    // set bird position using style relative to game area
+    birb.style.top = birdY + "px";
+    birb.style.left = birdX + "px";
 
     checkCollision();
     movePipes();
 
     requestAnimationFrame(gameLoop);
-  } else {
+  } else {  
     console.log("Game Over!");
   }
 };
 
-// Start
-setInterval(generatePipes, 4000);
+// inner text function when you lose the game
+const gameLostText = () => {
+  const gameLost = document.getElementById("gamelost");
+  if (isGameOver) {
+    gameLost.innerText = "Game Over! Refresh to play again.";
+  }
+};
+
+
+setInterval(generatePipes, 3000);
 requestAnimationFrame(gameLoop);
